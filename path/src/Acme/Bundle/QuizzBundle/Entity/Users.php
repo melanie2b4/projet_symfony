fcd461cf3f157b96d626ad6b9894aa6d72693b8c
Sonia pham <phamsonia@gmail.com>
2015-02-20 10:45:40 +0100

    Ajout des champs [score-cat] dans table users

--- path/src/Acme/Bundle/QuizzBundle/Entity/Users.php
+++ path/src/Acme/Bundle/QuizzBundle/Entity/Users.php
@@ -49,6 +49,75 @@ class Users
      */
     private $picture;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreSport", type="string", length=500)
+     */
+    private $scoreSport;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreDivertissements", type="string", length=500)
+     */
+    private $scoreDivertissements;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreNature", type="string", length=500)
+     */
+    private $scoreNature;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scorePolitique", type="string", length=500)
+     */
+    private $scorePolitique;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreHistoire", type="string", length=500)
+     */
+    private $scoreHistoire;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreHightech", type="string", length=500)
+     */
+    private $scoreHightech;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreGastronomie", type="string", length=500)
+     */
+    private $scoreGastronomie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreSante", type="string", length=500)
+     */
+    private $scoreSante;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreGeographie", type="string", length=500)
+     */
+    private $scoreGeographie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scoreCulturegenerale", type="string", length=500)
+     */
+    private $scoreCulturegenerale;
 
     /**
      * Get id
@@ -82,6 +151,7 @@ class Users
     {
         return $this->pseudo;
     }
+
 
     /**
      * Set mail
@@ -151,4 +221,238 @@ class Users
     {
         return $this->picture;
     }
+
+    /**
+     * Set scoreSport
+     *
+     * @param string $scoreSport
+     * @return Users
+     */
+    public function setScoreSport($scoreSport)
+    {
+        $this->scoreSport = $scoreSport;
+
+        return $this;
+    }
+
+    /**
+     * Get scoreSport
+     *
+     * @return string 
+     */
+    public function getScoreSport()
+    {
+        return $this->scoreSport;
+    }
+
+
+    /**
+     * Set scoreDivertissements
+     *
+     * @param string $scoreDivertissements
+     * @return Users
+     */
+    public function setScoreDivertissements($scoreDivertissements)
+    {
+        $this->scoreDivertissements = $scoreDivertissements;
+
+        return $this;
+    }
+
+    /**
+     * Get scoreDivertissements
+     *
+     * @return string 
+     */
+    public function getScoreDivertissements()
+    {
+        return $this->scoreDivertissements;
+    }
+
+    /**
+     * Set scoreNature
+     *
+     * @param string $scoreNature
+     * @return Users
+     */
+    public function setScoreNature($scoreNature)
+    {
+        $this->scoreNature = $scoreNature;
+
+        return $this;
+    }
+
+    /**
+     * Get scoreNature
+     *
+     * @return string 
+     */
+    public function getScoreNature()
+    {
+        return $this->scoreNature;
+    }
+
+    /**
+     * Set scorePolitique
+     *
+     * @param string $scorePolitique
+     * @return Users
+     */
+    public function setScorePolitique($scorePolitique)
+    {
+        $this->scorePolitique = $scorePolitique;
+
+        return $this;
+    }
+
+    /**
+     * Get scorePolitique
+     *
+     * @return string 
+     */
+    public function getScorePolitique()
+    {
+        return $this->scorePolitique;
+    }
+
+    /**
+     * Set scoreHistoire
+     *
+     * @param string $scoreHistoire
+     * @return Users
+     */
+    public function setScoreHistoire($scoreHistoire)
+    {
+        $this->scoreHistoire = $scoreHistoire;
+
+        return $this;
+    }
+
+    /**
+     * Get scoreHistoire
+     *
+     * @return string 
+     */
+    public function getScoreHistoire()
+    {
+        return $this->scoreHistoire;
+    }
+
+    /**
+     * Set scoreHightech
+     *
+     * @param string $scoreHightech
+     * @return Users
+     */
+    public function setScoreHightech($scoreHightech)
+    {
+        $this->scoreHightech = $scoreHightech;
+
+        return $this;
+    }
+
+    /**
+     * Get scoreHightech
+     *
+     * @return string 
+     */
+    public function getScoreHightech()
+    {
+        return $this->scoreHightech;
+    }
+
+    /**
+     * Set scoreGastronomie
+     *
+     * @param string $scoreGastronomie
+     * @return Users
+     */
+    public function setScoreGastronomie($scoreGastronomie)
+    {
+        $this->scoreGastronomie = $scoreGastronomie;
+
+        return $this;
+    }
+
+
+    /**
+     * Get scoreGastronomie
+     *
+     * @return string 
+     */
+    public function setScoreGastronomie()
+    {
+        return $this->scoreGastronomie;
+    }
+
+    /**
+     * Set scoreSante
+     *
+     * @param string $scoreSante
+     * @return Users
+     */
+    public function setScoreSante($scoreSante)
+    {
+        $this->scoreSante = $scoreSante;
+
+        return $this;
+    }
+
+    /**
+     * Get scoreSante
+     *
+     * @return string 
+     */
+    public function setScoreSante()
+    {
+        return $this->scoreSante;
+    }
+
+
+    /**
+     * Set scoreGeographie
+     *
+     * @param string $scoreGeographie
+     * @return Users
+     */
+    public function setScoreGeographie($scoreGeographie)
+    {
+        $this->scoreGeographie = $scoreGeographie;
+
+        return $this;
+    }
+
+    /**
+     * Get scoreGeographie
+     *
+     * @return string 
+     */
+    public function setScoreGeographie()
+    {
+        return $this->scoreGeographie;
+    }
+
+    /**
+     * Set scoreCulturegenerale
+     *
+     * @param string $scoreCulturegenerale
+     * @return Users
+     */
+    public function setScoreCulturegenerale($scoreCulturegenerale)
+    {
+        $this->scoreCulturegenerale = $scoreCulturegenerale;
+
+        return $this;
+    }
+
+    /**
+     * Get scoreCulturegenerale
+     *
+     * @return string 
+     */
+    public function setScoreCulturegenerale()
+    {
+        return $this->scoreCulturegenerale;
+    }
+
 }
