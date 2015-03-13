ff8da802977aaf322db8d9a857bb4df4d8ce1ba5
Sonia pham <phamsonia@gmail.com>
2015-03-13 15:10:11 +0100

    Utilisation des quizz hors connexion des users

--- path/src/Acme/Bundle/QuizzBundle/Controller/QuizzController.php
+++ path/src/Acme/Bundle/QuizzBundle/Controller/QuizzController.php
@@ -39,76 +39,81 @@ class QuizzController extends Controller
                 $score++;
             }
         }
-        if($quizz->getCategorie() == "Sport"){
-        $user = $this->getUser();
-        $user->setScoreSport($user->getScoreSport() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+        
+        if($this -> getUser() !== null){
 
-        if($quizz->getCategorie() == "Divertissement"){
-        $user = $this->getUser();
-        $user->setScoreDivertissements($user->getScoreDivertissements() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
 
-        if($quizz->getCategorie() == "Nature"){
-        $user = $this->getUser();
-        $user->setScoreNature($user->getScoreNaturee() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+            if($quizz->getCategorie() == "Sport"){
+            $user = $this->getUser();
+            $user->setScoreSport($user->getScoreSport() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
 
-        if($quizz->getCategorie() == "Politique"){
-        $user = $this->getUser();
-        $user->setScorePolitique($user->getScorePolitique() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+            if($quizz->getCategorie() == "Divertissement"){
+            $user = $this->getUser();
+            $user->setScoreDivertissements($user->getScoreDivertissements() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
 
-        if($quizz->getCategorie() == "Histoire"){
-        $user = $this->getUser();
-        $user->setScoreHistoire($user->getScoreHistoire() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+            if($quizz->getCategorie() == "Nature"){
+            $user = $this->getUser();
+            $user->setScoreNature($user->getScoreNaturee() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
 
-        if($quizz->getCategorie() == "Hightech"){
-        $user = $this->getUser();
-        $user->setScoreHightech($user->getScoreHightech() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+            if($quizz->getCategorie() == "Politique"){
+            $user = $this->getUser();
+            $user->setScorePolitique($user->getScorePolitique() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
 
-        if($quizz->getCategorie() == "Gastronomie"){
-        $user = $this->getUser();
-        $user->setScoreGastronomie($user->getScoreGastronomie() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+            if($quizz->getCategorie() == "Histoire"){
+            $user = $this->getUser();
+            $user->setScoreHistoire($user->getScoreHistoire() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
 
-        if($quizz->getCategorie() == "Sante"){
-        $user = $this->getUser();
-        $user->setScoreSante($user->getScoreSante() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+            if($quizz->getCategorie() == "Hightech"){
+            $user = $this->getUser();
+            $user->setScoreHightech($user->getScoreHightech() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
 
-        if($quizz->getCategorie() == "Geographie"){
-        $user = $this->getUser();
-        $user->setScoreGeographie($user->getScoreGeographie() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+            if($quizz->getCategorie() == "Gastronomie"){
+            $user = $this->getUser();
+            $user->setScoreGastronomie($user->getScoreGastronomie() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
 
-        if($quizz->getCategorie() == "Culturegenerale"){
-        $user = $this->getUser();
-        $user->setScoreCulturegenerale($user->getScoreCulturegenerale() + $score);
-        $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
-        }
+            if($quizz->getCategorie() == "Sante"){
+            $user = $this->getUser();
+            $user->setScoreSante($user->getScoreSante() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
 
+            if($quizz->getCategorie() == "Geographie"){
+            $user = $this->getUser();
+            $user->setScoreGeographie($user->getScoreGeographie() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
+
+            if($quizz->getCategorie() == "Culturegenerale"){
+            $user = $this->getUser();
+            $user->setScoreCulturegenerale($user->getScoreCulturegenerale() + $score);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            }
+
+        }
         return array(
             'score' => $score,
             'Quizz' => $quizz,
